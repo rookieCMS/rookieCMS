f6996ac6061e5d31c3c8ff6640c1c216333702de
JannikPC <jgrausen@gmail.com>
2016-02-23 17:34:19 +0100

    Schema setup login/passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,10 +23,25 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+        secret: 'fourHead',
+        resave: false,
+        saveUninitialized: false
+        }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+// passport configuration
+
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 
 
 // development database ---> when deployment fix to mongolab.
